f2b700656bba94d30d878e92105072c742816c12
nima80 <nima80@students.cs.ubc.ca>
2020-06-23 12:50:01 -0700

    Able to print up to date data from google spreadsheet

---
+++ create_sheet.py
@@ -0,0 +1,27 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from pprint import pprint
+
+scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+
+creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
+
+client = gspread.authorize(creds)
+
+sheet = client.open("API Call Test").sheet1  # Open the spreadhseet
+
+data = sheet.get_all_records()  # Get a list of all records
+
+pprint(data)
+
+#
+# row = sheet.row_values(3)  # Get a specific row
+# col = sheet.col_values(3)  # Get a specific column
+# cell = sheet.cell(1,2).value  # Get the value of a specific cell
+#
+# insertRow = ["hello", 5, "red", "blue"]
+# sheet.add_rows(insertRow, 4)  # Insert the list as a row at index 4
+#
+# sheet.update_cell(2,2, "CHANGED")  # Update one cell
+#
+# numRows = sheet.row_count  # Get the number of rows in the sheet
